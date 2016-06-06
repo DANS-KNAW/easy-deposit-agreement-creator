9bf9846f1f2770495553d82325e4e9ba842176e4
Richard van Heest <richard.van.heest@dans.knaw.nl>
2016-06-06 16:03:43 +0200

    same stuff... ???

--- src/main/scala/nl/knaw/dans/easy/license/PlaceholderMapper.scala
+++ src/main/scala/nl/knaw/dans/easy/license/PlaceholderMapper.scala
@@ -190,7 +190,6 @@ class PlaceholderMapper(metadataTermsFile: File)(implicit parameters: Parameters
 
 
       fedora.queryRiSearch(query)
-        .subscribeOn(IOScheduler())
         .flatMap(filePid => {
           val path = fedora.getFileMetadata(filePid)(_.loadXML \\ "path" text).subscribeOn(IOScheduler())
           val checksums = fedora.getFile(filePid)(_.getDsChecksum)
