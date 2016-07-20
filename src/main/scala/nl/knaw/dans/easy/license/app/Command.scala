DANS-KNAW/easy-deposit-agreement-creator

commit fe3bc6c75184268adc080970293d4a555f3dfae5

using Scala-ARM instead of our own resource cleanup system

src/main/scala/nl/knaw/dans/easy/license/app/Command.scala
@@ -15,11 +15,8 @@
  */
 package nl.knaw.dans.easy.license.app
 
-import java.io.FileOutputStream
-
 import nl.knaw.dans.easy.license.LicenseCreator
 import nl.knaw.dans.easy.license.app.{CommandLineOptions => cmd}
-import nl.knaw.dans.easy.license.internal.CloseableResourceExtensions
 import org.slf4j.LoggerFactory
 import rx.schedulers.Schedulers
 
@@ -34,8 +31,9 @@ object Command {
     try {
       implicit val (parameters, outputFile) = cmd.parse(args)
 
-      new FileOutputStream(outputFile)
-        .usedIn(LicenseCreator(parameters).createLicense)
+      resource.Using.fileOutputStream(outputFile)
+        .map(LicenseCreator(parameters).createLicense)
+        .acquireAndGet(identity)
         .doOnCompleted(log.info(s"license saved at ${outputFile.getAbsolutePath}"))
         .doOnTerminate {
           // close LDAP at the end of the main

src/main/scala/nl/knaw/dans/easy/license/internal/Fedora.scala
@@ -104,10 +104,11 @@ case class FedoraImpl(client: FedoraClient) extends Fedora {
   def this(credentials: FedoraCredentials) = this(new FedoraClient(credentials))
 
   private def query[T](pid: String, datastreamID: String)(f: InputStream => T): Observable[T] = {
-    FedoraClient.getDatastreamDissemination(pid, datastreamID)
-      .execute(client)
-      .getEntityInputStream
-      .usedIn(f.andThen(Observable.just(_)))
+    resource.managed {
+      FedoraClient.getDatastreamDissemination(pid, datastreamID)
+        .execute(client)
+        .getEntityInputStream
+    }.map(f).observe
   }
 
   def getAMD[T](pid: DatasetID)(f: (InputStream) => T) = query(pid, "AMD")(f).single
@@ -126,11 +127,11 @@ case class FedoraImpl(client: FedoraClient) extends Fedora {
   }
 
   def queryRiSearch(query: String) = {
-    Observable.defer {
+    resource.managed {
       new RiSearch(query).lang("sparql").format("csv").execute(client)
         .getEntityInputStream
-        .usedIn(is => Observable.from(Source.fromInputStream(is)
-          .getLines().toIterable).drop(1).map(_.split("/").last))
     }
+      .map(is => Observable.from(Source.fromInputStream(is).getLines().toIterable).drop(1).map(_.split("/").last))
+      .acquireAndGet(identity)
   }
 }

src/main/scala/nl/knaw/dans/easy/license/internal/package.scala
@@ -20,9 +20,10 @@ import java.nio.charset.Charset
 import java.util.Properties
 import javax.naming.NamingEnumeration
 
-import org.apache.commons.io.{Charsets, FileUtils, IOUtils}
+import org.apache.commons.io.{Charsets, FileUtils}
 import org.apache.commons.lang.StringUtils
 import org.slf4j.Logger
+import resource.ManagedResource
 import rx.lang.scala.Notification.{OnCompleted, OnError, OnNext}
 import rx.lang.scala.{Notification, Observable}
 
@@ -202,16 +203,15 @@ package object internal {
     })
   }
 
-  implicit class CloseableResourceExtensions[T <: Closeable](val resource: T) extends AnyVal {
-    def usedIn[S](observableFactory: T => Observable[S], dispose: T => Unit = _ => {}, disposeEagerly: Boolean = false): Observable[S] = {
-      Observable.using(resource)(observableFactory, t => { dispose(t); t.closeQuietly() }, disposeEagerly)
+  implicit class ReactiveResource[+T](val resource: ManagedResource[T]) extends AnyVal {
+    def observe: Observable[T] = {
+      try {
+        resource.acquireAndGet(Observable.just(_))
+      }
+      catch {
+        case e: Throwable => Observable.error(e)
+      }
     }
-
-    def use[S](f: T => S, dispose: T => Unit = _ => {}): Try[S] = {
-      Try(f(resource)).eventually(() => { dispose(resource); resource.closeQuietly() })
-    }
-
-    def closeQuietly() = IOUtils closeQuietly resource
   }
 
   implicit class InputStreamExtensions(val stream: InputStream) extends AnyVal {
@@ -243,11 +243,12 @@ package object internal {
   }
 
   def loadProperties(file: File): Try[Properties] = {
-    new FileInputStream(file)
-      .use(fis => {
+    resource.Using.fileInputStream(file)
+      .map(fis => {
         val props = new Properties
         props.load(fis)
         props
       })
+      .tried
   }
 }

pom.xml
@@ -90,6 +90,11 @@
 			<artifactId>velocity</artifactId>
 			<version>1.7</version>
 		</dependency>
+		<dependency>
+			<groupId>com.jsuereth</groupId>
+			<artifactId>scala-arm_2.11</artifactId>
+			<version>2.0-RC1</version>
+		</dependency>
 	</dependencies>
 	<repositories>
 		<repository>

src/main/scala/nl/knaw/dans/easy/license/LicenseCreator.scala
@@ -32,17 +32,17 @@ class LicenseCreator(placeholderMapper: PlaceholderMapper,
   val log = LoggerFactory.getLogger(getClass)
 
   def createLicense(dataset: Dataset)(outputStream: OutputStream): Try[Unit] = {
-    new ByteArrayOutputStream()
-      .use(templateOut => {
+    resource.managed(new ByteArrayOutputStream())
+      .map(templateOut => {
         log.info(s"""creating the license for dataset "${dataset.datasetID}"""")
         for {
           placeholders <- placeholderMapper.datasetToPlaceholderMap(dataset.validate)
           _ <- templateResolver.createTemplate(templateOut, placeholders)
           pdfInput = new ByteArrayInputStream(templateOut.toByteArray)
-          _ <- pdfInput.use(pdfGenerator.createPdf(_, outputStream).!)
+          _ <- resource.managed(pdfInput).map(pdfGenerator.createPdf(_, outputStream).!).tried
         } yield ()
       })
-      .flatten
+      .acquireAndGet(identity)
   }
 }
 

src/main/scala/nl/knaw/dans/easy/license/internal/TemplateResolver.scala
@@ -59,11 +59,13 @@ class VelocityTemplateResolver(properties: Properties)(implicit parameters: Base
   def createTemplate(out: OutputStream, map: PlaceholderMap, encoding: Charset = encoding) = {
     log.debug("resolve template placeholders")
 
-    new OutputStreamWriter(out, encoding).use(writer => {
-      val context = new VelocityContext
-      map.foreach { case (kw, o) => context.put(kw.keyword, o) }
+    resource.managed(new OutputStreamWriter(out, encoding))
+      .map(writer => {
+        val context = new VelocityContext
+        map.foreach { case (kw, o) => context.put(kw.keyword, o) }
 
-      engine.getTemplate(templateFileName, encoding.displayName()).merge(context, writer)
-    })
+        engine.getTemplate(templateFileName, encoding.displayName()).merge(context, writer)
+      })
+      .tried
   }
 }

commit ebf4f74919eeea235b087d2d3b298f33e47e3a32

fix closed stream bug introduced in fe3bc6c75184268adc080970293d4a555f3dfae5

src/main/scala/nl/knaw/dans/easy/license/app/Command.scala
@@ -15,8 +15,11 @@
  */
 package nl.knaw.dans.easy.license.app
 
+import java.io.FileOutputStream
+
 import nl.knaw.dans.easy.license.LicenseCreator
 import nl.knaw.dans.easy.license.app.{CommandLineOptions => cmd}
+import nl.knaw.dans.easy.license.internal._
 import org.slf4j.LoggerFactory
 import rx.schedulers.Schedulers
 
@@ -31,9 +34,8 @@ object Command {
     try {
       implicit val (parameters, outputFile) = cmd.parse(args)
 
-      resource.Using.fileOutputStream(outputFile)
-        .map(LicenseCreator(parameters).createLicense)
-        .acquireAndGet(identity)
+      new FileOutputStream(outputFile)
+        .usedIn(LicenseCreator(parameters).createLicense)
         .doOnCompleted(log.info(s"license saved at ${outputFile.getAbsolutePath}"))
         .doOnTerminate {
           // close LDAP at the end of the main

src/main/scala/nl/knaw/dans/easy/license/internal/Fedora.scala
@@ -106,11 +106,10 @@ case class FedoraImpl(client: FedoraClient) extends Fedora {
   def this(credentials: FedoraCredentials) = this(new FedoraClient(credentials))
 
   private def query[T](pid: String, datastreamID: String)(f: InputStream => T): Observable[T] = {
-    Observable.using {
-      FedoraClient.getDatastreamDissemination(pid, datastreamID)
-        .execute(client)
-        .getEntityInputStream
-    }(f.andThen(Observable.just(_)), IOUtils.closeQuietly)
+    FedoraClient.getDatastreamDissemination(pid, datastreamID)
+      .execute(client)
+      .getEntityInputStream
+      .usedIn(f.andThen(Observable.just(_)))
   }
 
   def getAMD[T](pid: DatasetID)(f: (InputStream) => T) = query(pid, "AMD")(f).single
@@ -129,11 +128,11 @@ case class FedoraImpl(client: FedoraClient) extends Fedora {
   }
 
   def queryRiSearch(query: String) = {
-    resource.managed {
+    Observable.defer {
       new RiSearch(query).lang("sparql").format("csv").execute(client)
         .getEntityInputStream
+        .usedIn(is => Observable.from(Source.fromInputStream(is)
+          .getLines().toIterable).drop(1).map(_.split("/").last))
     }
-      .map(is => Observable.from(Source.fromInputStream(is).getLines().toIterable).drop(1).map(_.split("/").last))
-      .acquireAndGet(identity)
   }
 }

src/main/scala/nl/knaw/dans/easy/license/internal/package.scala
@@ -20,7 +20,7 @@ import java.nio.charset.Charset
 import java.util.Properties
 import javax.naming.NamingEnumeration
 
-import org.apache.commons.io.{Charsets, FileUtils}
+import org.apache.commons.io.{Charsets, FileUtils, IOUtils}
 import org.apache.commons.lang.StringUtils
 import org.slf4j.Logger
 import rx.lang.scala.Notification.{OnCompleted, OnError, OnNext}
@@ -198,6 +198,12 @@ package object internal {
 //      }
 //    })
 //  }
+
+  implicit class ReactiveResourceManager[T <: Closeable](val resource: T) extends AnyVal {
+    def usedIn[S](observableFactory: T => Observable[S], dispose: T => Unit = _ => {}, disposeEagerly: Boolean = false): Observable[S] = {
+      Observable.using(resource)(observableFactory, t => { dispose(t); IOUtils.closeQuietly(t) }, disposeEagerly)
+    }
+  }
 
   implicit class InputStreamExtensions(val stream: InputStream) extends AnyVal {
     def loadXML = XML load stream